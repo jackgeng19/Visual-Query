6269d9632aef461ed9e89c52c0d858da427ce534
Jack Geng <103839282+jackgeng19@users.noreply.github.com>
2023-07-01 13:31:57 -0400

    crazy                 grouped_data[project_id][key]['narrations'] = []

--- ego4d_helper/groupby.py
+++ ego4d_helper/groupby.py
@@ -7,19 +7,46 @@ def groupby(out_path: str, input_path: str) -> None:
     with open(input_path, 'r') as file:
         data = json.load(file)
 
-    for key, value in data.items():
-        project_id = value['narrations'][0]['_project_id']
-        if project_id not in grouped_data:
-            grouped_data[project_id] = {}
-        grouped_data[project_id][key] = value
+    # project_ids = set()
 
-    with open(out_path, 'w') as outfile:
-        json.dump(grouped_data, outfile)
+    # for video_id, video_data in data.items():
+    #     narrations = video_data.get('narrations', [])
+    #     for narration in narrations:
+    #         project_id = narration.get('_project_id')
+    #         if project_id:
+    #             project_ids.add(project_id)
+
+    # print(list(project_ids))
+
+    for key, value in data.items():
+        i = 1
+        for item in value['narrations']:
+            project_id = item['_project_id']
+            # print(item)
+            if project_id not in grouped_data:
+                grouped_data[project_id] = {}
+                grouped_data[project_id][key] = value
+                print(grouped_data)
+                grouped_data[project_id][key]['narrations'] = [] 
+                print("nmsl")
+                print(grouped_data)
+                print(i)
+                print(project_id)
+
+            grouped_data[project_id][key]['narrations'].append(item)
+
+            print(f"{i}, {grouped_data[project_id][key]['narrations']}")
+            # print(">")
+            # print(grouped_data[project_id][key])
+            i += 1
+ 
+
+    with open(out_path, 'w') as file:
+        json.dump(grouped_data, file)
 
 
 def main() -> int:
-    groupby('groupby.json', 'QA_automation/out.json')
-    print("Grouped by project_id")
+    groupby('groupby.json', 'QA_automation/testing.json')
     return 0
 
 
